4bcba5e4175260339c469508f0670153fef5294d
Furqan Haroon <furqanh91@gmail.com>
2026-05-28 01:29:50 +0500

    Add iterative orchestration state management runtime

--- app/main.py
+++ app/main.py
@@ -7,9 +7,18 @@ from app.planner import decide_tool
 from app.logger import log_event
 
 app = FastAPI()
+MAX_ITERATIONS = 5
+
 
 class AgentRequest(BaseModel):
   query: str
+
+class AgentState(BaseModel):
+  user_query: str
+  current_iteration: int = 0
+  tool_history: list = []
+  observations: list = []
+  final_response: str | None = None
 
 @app.get("/health")
 async def health_check():
@@ -20,29 +29,56 @@ async def health_check():
 @app.post("/agent/run")
 async def run_agent(request: AgentRequest):
   query = request.query.lower()
-  tool_decider = decide_tool(query)
-  if tool_decider:
-    tool_name = tool_decider["tool"]
-    tool_arguments = tool_decider["arguments"]
-    tool = tool_registry[tool_name]
-    start_time = time.perf_counter()
-    tool_result = await tool(**tool_arguments)
-    execution_time = time.perf_counter()-start_time
-    log_event(
-      "tool_execution",
-      {
-        "tool_name": tool_name,
+  state = AgentState(user_query = query)
+  messages = [
+    {
+      "role": "user",
+      "content": query,
+    }
+  ]
+
+  while state.current_iteration < MAX_ITERATIONS:
+    log_event("iteration_started",{"iteration": state.current_iteration})
+    tool_decider = await decide_tool(messages)
+    print("\n tool_decider \n", tool_decider)
+    if tool_decider:
+      print("Tool decider details", tool_decider.name)
+      tool_name = tool_decider.name
+      tool_arguments = tool_decider.input
+      tool = tool_registry[tool_name]
+      start_time = time.perf_counter()
+      tool_result = await tool(**tool_arguments)
+      execution_time = time.perf_counter()-start_time
+      log_event(
+        "tool_execution",
+        {
+          "tool_name": tool_name,
+          "arguments": tool_arguments,
+          "tool_result": tool_result,
+          "execution_time_in_ms": execution_time
+        }
+      )
+      state.tool_history.append({
+        "tool": tool_name,
         "arguments": tool_arguments,
-        "tool_result": tool_result,
-        "execution_time_in_ms": execution_time
+        "tool_result": tool_result 
+      })
+      state.observations.append({
+        "tool": tool_name,
+        "observe": tool_result
+      })
+      messages.append({
+        "role": "user",
+        "content": f"Tool Result: {tool_result} "
+      })
+      state.current_iteration+=1
+      return {
+        "query": request.query,
+        "decision": tool_decider,
+        "tool_result": tool_result
       }
-    )
-    return {
-      "query": request.query,
-      "decision": tool_decider,
-      "tool_result": tool_result
-    }
-  else:
-    return {
-      "message": "No Matching Tool was found."
-    }
+    else:
+      state.final_response = "No action required"
+      return {
+        "message": "No Matching Tool was found."
+      }
--- app/planner.py
+++ app/planner.py
@@ -1,7 +1,7 @@
 from app.logger import log_event
-from anthropic import Anthropic
+from anthropic import AsyncAnthropic
 
-anthropic_client = Anthropic()
+anthropic_client = AsyncAnthropic()
 
 TOOLS=[
   {
@@ -32,19 +32,13 @@ TOOLS=[
   }
 ]
 
-def decide_tool(query:str):
-  query = query.lower()
-  llm_response = anthropic_client.messages.create(
+async def decide_tool(messages:list):
+  llm_response = await anthropic_client.messages.create(
     model="claude-sonnet-4-5",
     max_tokens=300,
     system="You are a helpful AI assistant. You job is to ONLY use the tool at your disposal. Pleae make sure to not use anyother external or hallucinate anyother requirements. If you don't find anything using the GIVEN tools just reply 'I wasn't able to find anything using the provided tools'",
     tools=TOOLS,
-    messages=[
-      {
-        "role": "user",
-        "content": query
-      }
-    ]
+    messages=messages
   )
   llm_content = llm_response.content
 
@@ -56,27 +50,4 @@ def decide_tool(query:str):
     if block.type == 'tool_use':
       tool_use = block
       break
-
-
-  # for block in llm
-
-
-  # if "cpu" in query:
-  #   decision = {
-  #     "tool": "get_cpu_usage",
-  #     "arguments": {
-  #       "server_name": "web-1"
-  #     }
-  #   }
-  #   log_event("planner_decision", decision)
-  #   return decision
-  # if "restart" in query:
-  #   decision = {
-  #     "tool_name": "restart_service",
-  #     "arguments": {
-  #       "service_name": "nginx"
-  #     }
-  #   }
-  #   log_event("planner_decision", decision)
-  #   return decision
-  # return None
+  return tool_use
