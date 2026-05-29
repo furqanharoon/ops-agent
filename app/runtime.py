76b52a63652181c45383d10a6a3d25d12dc5091c
Furqan Haroon <furqanh91@gmail.com>
2026-05-30 00:46:54 +0500

    Refactored runtime to move final response outside the while loop

--- app/runtime.py
+++ app/runtime.py
@@ -48,73 +48,7 @@ async def run_agent_execution(query):
     state.total_tokens=state.total_tokens+(state.total_input_tokens+state.total_output_tokens)
     state.llm_calls+=1
 
-    if tool_decider:
-      tool_name = tool_decider.name
-      tool_arguments = tool_decider.input
-      yield {
-        "event": "tool_selected",
-        "data": {
-          "tool_name": tool_name,
-          "tool_arguments": tool_arguments
-        }
-      }
-      tool = tool_registry[tool_name]
-      yield {
-        "event": "tool_exection",
-        "data":
-        {
-          "tool_name": tool_name,
-          "tool_arguments": tool_arguments
-        }
-      }
-      start_time = time.perf_counter()
-      tool_result = await tool(**tool_arguments)
-      execution_time = time.perf_counter()-start_time
-      log_event(
-        "tool_execution",
-        {
-          "tool_name": tool_name,
-          "arguments": tool_arguments,
-          "tool_result": tool_result,
-          "execution_time_in_ms": execution_time
-        }
-      )
-      state.tool_history.append({
-        "tool": tool_name,
-        "arguments": tool_arguments,
-        "tool_result": tool_result 
-      })
-      state.observations.append({
-        "tool": tool_name,
-        "observe": tool_result
-      })
-      yield {
-        "event": "observation",
-        "data": {
-          "tool_name": tool_name,
-          "tool_result": tool_result
-        }
-      }
-      messages.append({
-        "role": "user",
-        "content": f"Tool Result: {tool_result} "
-      })
-      state.current_iteration+=1
-      yield {
-        "event": "completed",
-        "data": {
-          "iterations": state.current_iteration,
-          "query": query,
-          "tool_history": state.tool_history,
-          "observations": state.observations
-        }
-      }
-      # return {
-      #   "query": query,
-      #   "decision": tool_decider,
-      #   "tool_result": tool_result
-      # }
-    else:
+    if not tool_decider:
       state.final_response = "No action required"
       yield {
         "event": "final_response",
@@ -122,6 +56,67 @@ async def run_agent_execution(query):
           "message": "No action required"
         }
       }
+
+    tool_name = tool_decider.name
+    tool_arguments = tool_decider.input
+    yield {
+      "event": "tool_selected",
+      "data": {
+        "tool_name": tool_name,
+        "tool_arguments": tool_arguments
+      }
+    }
+    tool = tool_registry[tool_name]
+    yield {
+      "event": "tool_exection",
+      "data":
+      {
+        "tool_name": tool_name,
+        "tool_arguments": tool_arguments
+      }
+    }
+    start_time = time.perf_counter()
+    tool_result = await tool(**tool_arguments)
+    execution_time = time.perf_counter()-start_time
+    log_event(
+      "tool_execution",
+      {
+        "tool_name": tool_name,
+        "arguments": tool_arguments,
+        "tool_result": tool_result,
+        "execution_time_in_ms": execution_time
+      }
+    )
+    state.tool_history.append({
+      "tool": tool_name,
+      "arguments": tool_arguments,
+      "tool_result": tool_result
+    })
+    state.observations.append({
+      "tool": tool_name,
+      "observe": tool_result
+    })
+    yield {
+      "event": "observation",
+      "data": {
+        "tool_name": tool_name,
+        "tool_result": tool_result
+      }
+    }
+    messages.append({
+      "role": "user",
+      "content": f"Tool Result: {tool_result} "
+    })
+    state.current_iteration+=1
+  yield {
+    "event": "completed",
+    "data": {
+      "iterations": state.current_iteration,
+      "query": query,
+      "tool_history": state.tool_history,
+      "observations": state.observations
+    }
+  }
 
 async def run_agent_execution_debug(query):
   state = AgentState(user_query=query)
@@ -141,44 +136,45 @@ async def run_agent_execution_debug(query):
     state.total_tokens=state.total_tokens+(state.total_input_tokens+state.total_output_tokens)
     state.llm_calls+=1
 
-    if tool_decider:
-      tool_name = tool_decider.name
-      tool_arguments = tool_decider.input
-      tool = tool_registry[tool_name]
-      start_time = time.perf_counter()
-      tool_result = await tool(**tool_arguments)
-      execution_time = time.perf_counter()-start_time
-      log_event(
-        "tool_execution",
-        {
-          "tool_name": tool_name,
-          "arguments": tool_arguments,
-          "tool_result": tool_result,
-          "execution_time_in_ms": execution_time
-        }
-      )
-      state.tool_history.append({
-        "tool": tool_name,
-        "arguments": tool_arguments,
-        "tool_result": tool_result 
-      })
-      state.observations.append({
-        "tool": tool_name,
-        "observe": tool_result
-      })
-      messages.append({
-        "role": "user",
-        "content": f"Tool Result: {tool_result} "
-      })
-      state.current_iteration+=1
-      return {
-        "query": query,
-        "decision": tool_decider,
-        "tool_result": tool_result,
-        "state": state
-      }
-    else:
+    if not tool_decider:
       state.final_response = "No action required"
       return {
         "message": "No Matching Tool was found."
       }
+
+    tool_name = tool_decider.name
+    tool_arguments = tool_decider.input
+    tool = tool_registry[tool_name]
+    start_time = time.perf_counter()
+    tool_result = await tool(**tool_arguments)
+    execution_time = time.perf_counter()-start_time
+    log_event(
+      "tool_execution",
+      {
+        "tool_name": tool_name,
+        "arguments": tool_arguments,
+        "tool_result": tool_result,
+        "execution_time_in_ms": execution_time
+      }
+    )
+    state.tool_history.append({
+      "tool": tool_name,
+      "arguments": tool_arguments,
+      "tool_result": tool_result
+    })
+    state.observations.append({
+      "tool": tool_name,
+      "observe": tool_result
+    })
+    messages.append({
+      "role": "user",
+      "content": f"Tool Result: {tool_result} "
+    })
+    state.current_iteration+=1
+  
+  return {
+    "query": query,
+    "decision": tool_decider,
+    "tool_result": tool_result,
+    "state": state
+  }
