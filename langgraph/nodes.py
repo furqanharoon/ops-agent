ee81abbb2dfaf4715cb93a22931ed9676a7072ab
Furqan Haroon <furqanh91@gmail.com>
2026-06-06 23:31:25 +0500

    WIP: Implementing LangGraph updated Workflow with Interruptions and Checkpoints

--- langgraph/nodes.py
+++ langgraph/nodes.py
@@ -1,8 +1,26 @@
 from langgraph.state import WorkflowState
+from app.schemas.investigation_facts import InvestigationFacts
+from app.services.analyzer import analyze_incident
 
 def analysis_node(state: WorkflowState):
   print("\n Running Analysis Node \n")
-  state['analysis'] = "Analysis Done"
+  state['analysis'] = analyze_incident(state['facts'])
+  return state
+
+def facts_node(state: WorkflowState):
+  print("\n Running Facts Node \n")
+  state['facts'] = InvestigationFacts(
+    case_id="INC24493",
+    priority="Low",
+    reporter="John",
+    final_resolver="Sarah",
+    customer_satisfaction=1,
+    resolution_duration_hours=52.3,
+    total_personnel=7,
+    number_of_handoffs=9,
+    support_levels_involved=3,
+    escalation_count=3
+  )
   return state
 
 def report_node(state: WorkflowState):
@@ -12,3 +30,12 @@ def report_node(state: WorkflowState):
     {state['analysis']}
   """
   return state
+
+def approval_node(state: WorkflowState):
+  print("\n Running Approval Node \n")
+  return state
+  
+def route_after_analysis(state: WorkflowState):
+  if state["analysis"].severity == "high":
+    return "approval"
+  return "report"
--- langgraph/workflow.py
+++ langgraph/workflow.py
@@ -1,10 +1,14 @@
 from langgraph.graph import StateGraph,START,END
 
 from langgraph.state import WorkflowState
-from langgraph.nodes import analysis_node, report_node
+from langgraph.nodes import analysis_node, report_node, approval_node, route_after_analysis, facts_node
 
 graph_builder = StateGraph(WorkflowState) # Creates an empty graph
 
+graph_builder.add_node(
+  "facts",
+  facts_node
+)
 graph_builder.add_node(
   "analysis",
   analysis_node
@@ -14,20 +18,37 @@ graph_builder.add_node(
   "report",
   report_node
 )
+graph_builder.add_node(
+  "approval",
+  approval_node
+)
 
 graph_builder.add_edge(
   START,
-  "analysis"
+  "facts"
 ) # This adds and Edge that says START and goto analysis node
 
+# graph_builder.add_edge(
+#   "analysis",
+#   "report"
+# )
 graph_builder.add_edge(
+  "facts",
+  "analysis"
+)
+graph_builder.add_conditional_edges(
   "analysis",
+  route_after_analysis
+)
+
+graph_builder.add_edge(
+  "approval",
   "report"
 )
 
 graph_builder.add_edge(
   "report",
   END
-) # This add another Edge that says from Analysis node, goto END of Graph.
+) # This add axnother Edge that says from Analysis node, goto END of Graph.
 
 graph = graph_builder.compile() # This run the whole graph and make it executable
