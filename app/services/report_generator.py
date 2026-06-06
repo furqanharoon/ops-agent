9e704e40535e0cc9212651f174e0bd50cd44278a
Furqan Haroon <furqanh91@gmail.com>
2026-06-06 23:50:34 +0500

    Add EOL to report generator and removed unused commented code

--- app/services/report_generator.py
+++ app/services/report_generator.py
@@ -1,9 +1,6 @@
 from app.schemas.investigation_facts import InvestigationFacts
 from app.schemas.incident_analysis import IncidentAnalysis
 
-#  FACTSSSSSS case_id='INC24493' priority='Low' reporter='John' final_resolver='sarah' customer_satisfaction=1 resolution_duration_hours=56.31666666666667 total_personnel=7 number_of_handoffs=9 support_levels_involved=3 escalation_count=3 error_message=''
-
-
 def generate_report(facts: InvestigationFacts, analysis:IncidentAnalysis) -> str:
   return f"""
     Case ID: {facts.case_id}
