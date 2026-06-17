8e9192e1b37449db3e3bdeafb7d9816ea7f5da6b
Furqan Haroon <furqanh91@gmail.com>
2026-06-17 21:51:08 +0500

    Moved workflow core logic to LangGraph services

---
+++ langgraph/services/workflow_service.py
@@ -0,0 +1,75 @@
+import asyncio
+import uuid
+from langgraph.workflow import get_graph
+from langgraph.types import Command
+from agent.runtime import run_agent_execution_debug
+from langgraph.checkpoint.postgres import PostgresSaver
+from langgraph.workflow_runs import create_workflow_run
+
+DB_URI = "postgresql://postgres@localhost/incident_management"
+
+async def start_workflow(case_id: str):
+  query = f"Investigate {case_id}"
+
+  response = await run_agent_execution_debug(query)
+  
+  result = None
+
+  thread_id = str(uuid.uuid4())
+  create_workflow_run(thread_id)
+  print(f"Thread ID: {thread_id}")
+  with PostgresSaver.from_conn_string(DB_URI) as checkpointer:
+    graph = get_graph(checkpointer)
+
+    result = graph.invoke(
+      {
+        "thread_id": thread_id,
+        "incident": response['incident'],
+        "duration": response['duration'],
+        "timeline": response['timeline'],
+        "facts": None,
+        "analysis": None,
+        "report": None,
+        "approval_status": None,
+        "rejection_status": None
+      },
+      config={
+        "configurable": {
+          "thread_id": thread_id
+        }
+      }
+    )
+    print(result)
+
+  return {
+    "thread_id": thread_id,
+    "state": result
+  }
+
+async def resume_workflow(
+  thread_id:str,
+  approval_status:str | None = None,
+  rejection_reason:str | None = None
+):
+  resume_payload = {
+    "approval_status": approval_status
+  }
+  if rejection_reason:
+    resume_payload["rejection_reason"] = rejection_reason
+  with PostgresSaver.from_conn_string(DB_URI) as checkpointer:
+    graph = get_graph(checkpointer)
+    resume_result = graph.invoke(
+      Command(
+        resume=resume_payload
+      ),
+      config = {
+        "configurable": {
+          "thread_id": thread_id
+        }
+      }
+    )
+    print(resume_result)
+  
+  return {
+    "state": resume_result
+  }
